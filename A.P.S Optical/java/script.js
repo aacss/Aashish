e52325c745c24dd89d3080338da6a5732a9106d9
aacss <88428838+aacss@users.noreply.github.com>
2021-08-04 18:04:32 +0545

    Add files via upload

---
+++ A.P.S Optical/java/script.js
@@ -0,0 +1,15 @@
+
+	function checkempty()
+	{
+		alert("Thank you for submitting");
+	}
+
+	function video()
+	{
+		alert("Thanks for watching");
+	}
+
+	function music()
+	{
+		alert("Thanks for listening")
+	}
